8a52e7b411ac780be5b8a77f60ea09dea44c23aa
Moesbi <jojo268@gmx.net>
2021-05-30 18:51:45 +0200

    Update CharacterLogic.cs

--- Prototype Virus Game/GameLogic/CharacterLogic.cs
+++ Prototype Virus Game/GameLogic/CharacterLogic.cs
@@ -11,56 +11,124 @@ namespace Prototype_Virus_Game
 {
     public class CharacterLogic
     {
+        int x =100;
+        int y=495;
         public void Logic(object sender, EventArgs e)
         {
             if (GameState.Jump)
             {
-                UiComponents.Character.Top -= 13;
-            }
-            else if (GameState.Jump == false)
-            {
-                if (GameState.OnPlatform == false)
+                if (!(Game.instance.pbCharacterBounds.Location.Y < GameState.GroundLevel) || GameState.OnPlatform)
                 {
-                    if (UiComponents.Character.Location.Y < GameState.GroundLevel)
-                        UiComponents.Character.Top += 10;
+                    Graphics gb = Graphics.FromImage(Game.instance.anzeige);  
+                gb.DrawImage(Game.instance.hinterbmp1, 0, 0, Game.instance.hinterbmp1.Width, Game.instance.hinterbmp1.Height); 
+                gb.DrawImage(Game.instance.platform, Game.instance.x, Game.instance.y, 326, 95);
+                gb.DrawImage(Game.instance.platform, 550, 350, 326, 95);  
+                gb.DrawImage(Game.instance.character, x, y-160, 103, 167);
+                gb.Dispose();                       
+                Game.instance.pbBackGround.Image = Game.instance.anzeige;
+
+                Game.instance.pbCharacterBounds.Top -= 160;
+                y -= 160;
+
+                }
+
+            }
+
+            if (GameState.OnPlatform == false)
+            {
+                if (Game.instance.pbCharacterBounds.Location.Y < GameState.GroundLevel)
+                {
+                    Graphics gb = Graphics.FromImage(Game.instance.anzeige);
+                    gb.DrawImage(Game.instance.hinterbmp1, 0, 0, Game.instance.hinterbmp1.Width, Game.instance.hinterbmp1.Height);
+                    gb.DrawImage(Game.instance.platform, Game.instance.x, Game.instance.y, 326, 95);
+                    gb.DrawImage(Game.instance.platform, 550, 350, 326, 95);
+                    gb.DrawImage(Game.instance.character, x, y + 10, 103, 167);
+                    gb.Dispose();
+                    Game.instance.pbBackGround.Image = Game.instance.anzeige;
+
+                    Game.instance.pbCharacterBounds.Top += 10;
+                    y += 10;
+
                 }
             }
 
+
+
+
             if (GameState.RunRight)
             {
-                if (UiComponents.Character.Location.X < UiComponents.BackGround.ClientSize.Width - 100)
+                if (Game.instance.pbCharacterBounds.Location.X < UiComponents.BackGround.ClientSize.Width - 105)
                 {
-                    UiComponents.Character.Left += 13;
+                   
+
+                    Graphics gb = Graphics.FromImage(Game.instance.anzeige);  
+                    gb.DrawImage(Game.instance.hinterbmp1, 0, 0, Game.instance.hinterbmp1.Width, Game.instance.hinterbmp1.Height); 
+                    gb.DrawImage(Game.instance.platform, Game.instance.x, Game.instance.y, 326, 95);
+                    gb.DrawImage(Game.instance.platform, 550, 350, 326, 95);
+                    gb.DrawImage(Game.instance.character, x+13,y, 103, 167);
+                    gb.Dispose(); 
+                    Game.instance.pbBackGround.Image = Game.instance.anzeige;
+                    
+
+                    Game.instance.pbCharacterBounds.Left += 13;
+                    x += 13;
+
                 }
             }
 
             if (GameState.RunLeft)
             {
-                if (UiComponents.Character.Location.X > 0)
-                    UiComponents.Character.Left -= 13;
+                if (Game.instance.pbCharacterBounds.Location.X  > 0)
+                {
+
+                Graphics gb = Graphics.FromImage(Game.instance.anzeige);  
+                gb.DrawImage(Game.instance.hinterbmp1, 0, 0, Game.instance.hinterbmp1.Width, Game.instance.hinterbmp1.Height); 
+                gb.DrawImage(Game.instance.platform, Game.instance.x, Game.instance.y, 326, 95);
+                gb.DrawImage(Game.instance.platform, 550, 350, 326, 95);
+                gb.DrawImage(Game.instance.character, x - 13, y, 103, 167);
+                gb.Dispose();                            
+                Game.instance.pbBackGround.Image = Game.instance.anzeige;
+
+                Game.instance.pbCharacterBounds.Left -= 13;
+                x -= 13;
+                }
+                    
+
+               
+
             }
 
             if (GameState.TurnPlayer)
             {
                 if (GameState.RunLeft && GameState.PlayerLookingRight)
                 {
-                    UiComponents.Character.Image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                    Game.instance.character = Game.instance.characterLeft;
                     GameState.PlayerLookingRight = false;
                     GameState.PlayerLookingLeft = true;
                 }
                 else if (GameState.RunRight && GameState.PlayerLookingLeft)
                 {
-                    UiComponents.Character.Image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                    Game.instance.character = Game.instance.characterRight;
                     GameState.PlayerLookingLeft = false;
                     GameState.PlayerLookingRight = true;
                 }
             }
 
 
-            if (UiComponents.Character.Bounds.IntersectsWith(UiComponents.Platform.Bounds))
+            if (Game.instance.pbCharacterBounds.Bounds.IntersectsWith(Game.instance.pbPlatform1.Bounds))
             {
-                if (UiComponents.Character.Bottom > UiComponents.Platform.Top - 10 && UiComponents.Character.Bottom < UiComponents.Platform.Top + 10)
+                int playerFeet = Game.instance.pbCharacterBounds.Location.Y + Game.instance.pbCharacterBounds.Height;
+
+                if (playerFeet > Game.instance.pbPlatform1.Bounds.Top - 5 && playerFeet < Game.instance.pbPlatform1.Bounds.Top + 5)
                 {
+                    Graphics gb = Graphics.FromImage(Game.instance.anzeige);
+                    gb.DrawImage(Game.instance.hinterbmp1, 0, 0, Game.instance.hinterbmp1.Width, Game.instance.hinterbmp1.Height);
+                    gb.DrawImage(Game.instance.platform, Game.instance.x, Game.instance.y, 326, 95);
+                    gb.DrawImage(Game.instance.platform, 550, 350, 326, 95);
+                    gb.DrawImage(Game.instance.character, Game.instance.pbCharacterBounds.Location.X, Game.instance.pbPlatform1.Top- 163, 103, 167);
+                    gb.Dispose();
+                    Game.instance.pbBackGround.Image = Game.instance.anzeige;
+
                     GameState.OnPlatform = true;
                 }
             }
@@ -68,6 +136,29 @@ namespace Prototype_Virus_Game
             {
                 GameState.OnPlatform = false;
             }
+
+            //if (Game.instance.pbCharacterBounds.Bounds.IntersectsWith(Game.instance.pbPlatform2.Bounds))
+            //{
+            //    int playerFeet = Game.instance.pbCharacterBounds.Location.Y + Game.instance.pbCharacterBounds.Height;
+
+            //    if (playerFeet > Game.instance.pbPlatform2.Bounds.Top - 5 && playerFeet < Game.instance.pbPlatform2.Bounds.Top + 5)
+            //    {
+            //        Graphics gb = Graphics.FromImage(Game.instance.anzeige);
+            //        gb.DrawImage(Game.instance.hinterbmp1, 0, 0, Game.instance.hinterbmp1.Width, Game.instance.hinterbmp1.Height);
+            //        gb.DrawImage(Game.instance.platform, Game.instance.x, Game.instance.y, 326, 95);
+            //        gb.DrawImage(Game.instance.platform, 550, 350, 326, 95);
+            //        gb.DrawImage(Game.instance.character, Game.instance.pbCharacterBounds.Location.X, Game.instance.pbPlatform2.Top - 163, 103, 167);
+            //        gb.Dispose();
+            //        Game.instance.pbBackGround.Image = Game.instance.anzeige;
+
+            //        GameState.OnPlatform = true;
+            //    }
+            //}
+            //else
+            //{
+            //    GameState.OnPlatform = false;
+            //}
+
         }
     }
 }
