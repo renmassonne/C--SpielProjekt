510e8f4bac862f01824fb73246158510617f5637
Moesbi <jojo268@gmx.net>
2021-06-04 12:25:25 +0200

    Update Bullet.cs

--- Prototype Virus Game/GameLogic/Bullet.cs
+++ Prototype Virus Game/GameLogic/Bullet.cs
@@ -12,7 +12,7 @@ namespace Prototype_Virus_Game.GameLogic
     {
 
         PictureBox bullet;
-        Timer bulletTimer = new Timer();
+        Timer bulletTimer = new Timer();       
 
         public void CreateProjectile()
         {
@@ -20,13 +20,12 @@ namespace Prototype_Virus_Game.GameLogic
             bullet.Size = new Size(15, 15);
             bullet.SizeMode = PictureBoxSizeMode.StretchImage;
             bullet.Image = global::Prototype_Virus_Game.Properties.Resources.BlueDot;
-            Game.instance.Controls.Add(bullet);
+            //Game.instance.Controls.Add(bullet);
+           
+            bullet.BackColor = Color.Transparent;
+            bullet.Parent = Game.instance.pbBackGround;
 
             bullet.BringToFront();
-
-
-
-
 
 
             if (GameState.PlayerLookingRight)
@@ -38,6 +37,7 @@ namespace Prototype_Virus_Game.GameLogic
             {
                 bullet.Location = new Point(Game.instance.pbCharacterBounds.Location.X - 20, Game.instance.pbCharacterBounds.Location.Y + 95);
             }
+
             bulletTimer.Interval = 20;
             bulletTimer.Tick += new EventHandler(bulletTimerTick);
             bulletTimer.Start();
@@ -46,16 +46,36 @@ namespace Prototype_Virus_Game.GameLogic
         public void bulletTimerTick(object sender, EventArgs e)
         {
 
+            foreach (var virus in UiComponents.Viruses)
+            {                           
+                    if (virus.Bounds.IntersectsWith(bullet.Bounds))
+                    {
+                        GameState.VirusHit = true;
+                        //UiComponents.RemoveVirus(virus);
+                        Game.instance.Controls.Remove(virus);
+                        virus.Dispose();
+                        
+                    }
+                    else
+                    {
+                        GameState.VirusHit = false;
+                    }
+                
+            }
 
             if (bullet.Location.X > Game.instance.pbCharacterBounds.Location.X)
             {
-                if (!(bullet.Left > Game.instance.pbBackGround.Width))
+                if (bullet.Left < Game.instance.pbBackGround.Width)
+                {
                     bullet.Left += 20;
+
+                }
                 else
                 {
                     bulletTimer.Stop();
                     bulletTimer.Dispose();
-                    Game.instance.Controls.Remove(bullet);
+                    //Game.instance.Controls.Remove(bullet);
+                    
                     bullet.Dispose();
                     bulletTimer = null;
                     bullet = null;
@@ -65,20 +85,33 @@ namespace Prototype_Virus_Game.GameLogic
             else if (bullet.Location.X < Game.instance.pbCharacterBounds.Location.X)
             {
                 if (bullet.Left > 0)
+                {
                     bullet.Left -= 20;
+                }
+
                 else
                 {
                     bulletTimer.Stop();
                     bulletTimer.Dispose();
-                    Game.instance.Controls.Remove(bullet);
+                    //Game.instance.Controls.Remove(bullet);                  
                     bullet.Dispose();
                     bulletTimer = null;
                     bullet = null;
-
-
                 }
 
             }
+
+            if (GameState.VirusHit)
+            {
+                bulletTimer.Stop();
+                bulletTimer.Dispose();
+                //Game.instance.Controls.Remove(bullet);              
+                bullet.Dispose();
+                bulletTimer = null;
+                bullet = null;
+
+            }
+            
 
         }
     }
