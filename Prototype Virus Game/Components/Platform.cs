458b9ea3a3eaadbf1fef40497ecee7ff05cb430a
Moesbi <jojo268@gmx.net>
2021-06-04 12:25:01 +0200

    Create Platform.cs

---
+++ Prototype Virus Game/Components/Platform.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototype_Virus_Game.Components
+{
+    public class GamePlatform : PictureBox
+    {
+       
+        public GamePlatform(Rectangle bounds)
+        {
+            Name = "pbPlatform";
+            TabIndex = 6;
+            TabStop = false;
+            Size = new Size(bounds.Width, bounds.Height);         
+            Location = new Point(bounds.X, bounds.Y);
+            Visible = false;        
+        }
+
+        
+    }
+}
