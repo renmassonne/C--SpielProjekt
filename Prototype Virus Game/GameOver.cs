0dea5df5a60519b2888f9b6ee203ac1656e78a5e
Moesbi <jojo268@gmx.net>
2021-06-05 19:56:28 +0200

    Create GameOver.cs

---
+++ Prototype Virus Game/GameOver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototype_Virus_Game
+{
+    public partial class GameOver : Form
+    {
+        public GameOver()
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.None;
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
