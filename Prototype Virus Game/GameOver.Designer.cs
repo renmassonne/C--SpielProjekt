d768062bdb747e36f3889d0c352f50241341cc10
Moesbi <jojo268@gmx.net>
2021-06-07 21:40:03 +0200

    Update GameOver.Designer.cs

--- Prototype Virus Game/GameOver.Designer.cs
+++ Prototype Virus Game/GameOver.Designer.cs
@@ -37,7 +37,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe Print", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(166, 93);
+            this.label1.Location = new System.Drawing.Point(113, 86);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(472, 85);
             this.label1.TabIndex = 0;
@@ -48,19 +48,20 @@
             this.btnBackToMenu.BackColor = System.Drawing.Color.DarkOrchid;
             this.btnBackToMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnBackToMenu.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBackToMenu.Location = new System.Drawing.Point(112, 252);
+            this.btnBackToMenu.Location = new System.Drawing.Point(75, 252);
             this.btnBackToMenu.Name = "btnBackToMenu";
             this.btnBackToMenu.Size = new System.Drawing.Size(227, 72);
             this.btnBackToMenu.TabIndex = 1;
-            this.btnBackToMenu.Text = "Zurück zum Menü";
+            this.btnBackToMenu.Text = "Spiel beenden";
             this.btnBackToMenu.UseVisualStyleBackColor = false;
+            this.btnBackToMenu.Click += new System.EventHandler(this.btnBackToMenu_Click);
             // 
             // btnRestart
             // 
             this.btnRestart.BackColor = System.Drawing.Color.DarkOrchid;
             this.btnRestart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnRestart.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRestart.Location = new System.Drawing.Point(449, 251);
+            this.btnRestart.Location = new System.Drawing.Point(415, 252);
             this.btnRestart.Name = "btnRestart";
             this.btnRestart.Size = new System.Drawing.Size(227, 73);
             this.btnRestart.TabIndex = 2;
@@ -73,7 +74,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SlateGray;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(723, 432);
             this.Controls.Add(this.btnRestart);
             this.Controls.Add(this.btnBackToMenu);
             this.Controls.Add(this.label1);
