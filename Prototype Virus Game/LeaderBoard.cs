2beb469f93feece9ec247a5acf24fb755678ae84
Moesbi <jojo268@gmx.net>
2021-06-07 21:40:06 +0200

    Create LeaderBoard.cs

---
+++ Prototype Virus Game/LeaderBoard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototype_Virus_Game
+{
+    public partial class LeaderBoard : Form
+    {
+        public LeaderBoard()
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.None;
+
+
+        }
+    }
+}
