46d969a8756443b9b306f3b6a572110a6ba9ed51
Moesbi <jojo268@gmx.net>
2021-06-15 19:59:07 +0200

    Update GameOver.cs

--- Prototype Virus Game/GameOver.cs
+++ Prototype Virus Game/GameOver.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,23 +16,16 @@ namespace Prototype_Virus_Game
         {
             InitializeComponent();
             FormBorderStyle = FormBorderStyle.None;
-            
-           
         }
-        
 
         private void btnRestart_Click(object sender, EventArgs e)
         {
             Application.Restart();
-            var highScoreLogic = new HighScoreLogic();
-            highScoreLogic.Save();
         }
 
         private void btnBackToMenu_Click(object sender, EventArgs e)
         {
             Game.instance.Close();
-            var highScoreLogic = new HighScoreLogic();
-            highScoreLogic.Save();
         }
     }
 }
