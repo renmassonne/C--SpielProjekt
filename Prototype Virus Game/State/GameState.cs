b95eb1ba57825fa53fc69e1f98694461559c9516
Moesbi <jojo268@gmx.net>
2021-06-15 19:59:47 +0200

    Update GameState.cs

--- Prototype Virus Game/State/GameState.cs
+++ Prototype Virus Game/State/GameState.cs
@@ -22,14 +22,15 @@ namespace Prototype_Virus_Game
 
         public static bool OnPlatform { get; set; }
         public static bool JumpDown { get; set; }
-
-        public static bool Fullscreen { get; set; }
+        
         public static int GroundLevel { get; set; }
-        public static bool PlayerGotHit {get; set;}
-        public static bool VirusHit { get; set; }     
-        public static int playerHealth { get; set; } = 3;
-        public static int HighScore { get; set; }
-        public static List<HighScoreEntry> HighScoreList { get; set; }
+        public static bool PlayerGotHit { get; set; }
+        public static bool VirusHit { get; set; }
+        public static int PlayerHealth { get; set; } = 3;
+        public static int Score { get; set; }
+        public static int VirusKillCount { get; set; }
+        public static int HeartDroppingCounter { get; set; } = 10;     
+        public static bool BossFight { get; set; } 
 
         static GameState()
         {
