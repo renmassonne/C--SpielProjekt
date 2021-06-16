6d61bc14ba26b37adf3b64e4722dbf529fa30171
Moesbi <jojo268@gmx.net>
2021-06-16 13:44:46 +0200

    Update LeaderBoard.Designer.cs

--- Prototype Virus Game/LeaderBoard.Designer.cs
+++ Prototype Virus Game/LeaderBoard.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Prototype_Virus_Game
 {
-    partial class LeaderBoard
+    partial class Leaderboard
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbScore = new System.Windows.Forms.ListBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbScore
+            // 
+            this.lbScore.BackColor = System.Drawing.Color.SlateGray;
+            this.lbScore.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lbScore.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbScore.ForeColor = System.Drawing.Color.Black;
+            this.lbScore.FormattingEnabled = true;
+            this.lbScore.ItemHeight = 35;
+            this.lbScore.Location = new System.Drawing.Point(12, 123);
+            this.lbScore.Name = "lbScore";
+            this.lbScore.Size = new System.Drawing.Size(534, 385);
+            this.lbScore.TabIndex = 0;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(200, 575);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(145, 52);
+            this.btnBack.TabIndex = 1;
+            this.btnBack.Text = "zurück";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkOrchid;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(126, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 87);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Highscores";
+            // 
+            // Leaderboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(558, 639);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.lbScore);
+            this.Name = "Leaderboard";
             this.Text = "LeaderBoard";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lbScore;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label label1;
     }
 }
